e685c41b33d76bb9c9be5d31d290077d52701ed6
Satyajit Nayak <satyajitnayak348@gmail.com>
2024-03-29 21:10:17 +0530

    express basic set

--- server/server.js
+++ server/server.js
@@ -1,1 +1,15 @@
-console.log('Hello world');
+const express = require('express');
+
+const app = express();
+
+app.get('/api/signup', (req, res) => {
+    res.json({
+        data: 'you hit signup endoin yay'
+    })
+});
+
+const PORT = process.env.PORT || 4500;
+app.listen(PORT, () => {
+    console.log(`Server running: http://localhost:${PORT}/`)
+})
+
